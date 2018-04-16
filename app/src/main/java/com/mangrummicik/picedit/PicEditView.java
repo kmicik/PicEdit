8ce4e401a5dc50a68b50e444ffdcfbee18194182
kmicik <kmicik>
2018-04-16 03:26:05 -0500

    Initialize variables in MainActivity, Constructor in custom class

---
+++ app/src/main/java/com/mangrummicik/picedit/PicEditView.java
@@ -0,0 +1,26 @@
+package com.mangrummicik.picedit;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PicEditView extends View {
+    public PicEditView(Context context) {
+        super(context);
+        this.init();
+    }
+
+    public PicEditView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.init();
+    }
+
+    public PicEditView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.init();
+    }
+
+    private void init() {
+        
+    }
+}
