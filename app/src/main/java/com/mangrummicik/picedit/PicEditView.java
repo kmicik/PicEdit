a0169492194fdbb1a652b08bcf9ad517533d9648
kmicik <kmicik>
2018-04-17 02:16:59 -0500

    lines and rectangles

--- app/src/main/java/com/mangrummicik/picedit/PicEditView.java
+++ app/src/main/java/com/mangrummicik/picedit/PicEditView.java
@@ -1,6 +1,7 @@
 package com.mangrummicik.picedit;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -19,18 +20,21 @@ public class PicEditView extends View {
     private final int MAX_SIZE = 100;
     private final int MIN_SIZE = 5;
     private final int DEFAULT_COLOR = Color.WHITE;
+    private final int DEFAULT_SHAPE = 1;
     public static final int LINE = 1;
     public static final int RECTANGLE = 2;
     public static final int CIRCLE = 3;
     private int mPenColor;
     public int mCurrentShape;
+    protected Canvas mCanvas;
+    protected Bitmap mBitmap;
 
     private ArrayList<Path> mPaths;
     private ArrayList<Paint> mPaints;
 
     private Path mPath;
     private Paint mPaint;
-    private float mX, mY;
+    private float mX, mY, oldMx, oldMy;
 
     public PicEditView(Context context) {
         super(context);
@@ -47,15 +51,23 @@ public class PicEditView extends View {
         this.init();
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
     private void init() {
 
         this.mSize = DEFAULT_SIZE;
         this.mPenColor = DEFAULT_COLOR;
+        this.mCurrentShape = DEFAULT_SHAPE;
         this.mPaths = new ArrayList<Path>();
         this.mPaints = new ArrayList<Paint>();
         this.mPath = new Path();
         this.addPath();
-        this.mX = mY = (float)0.0;
+        this.mX = mY = oldMx = oldMy =(float)0.0;
 
     }
 
@@ -147,11 +159,11 @@ public class PicEditView extends View {
 
     private void onDrawLine(Canvas canvas) {
 
-        for (int i = 0; i < mPaths.size(); ++i) {
+        for (int i = 0; i < mPaths.size(); ++i)
 
             canvas.drawPath(mPaths.get(i), mPaints.get(i));
 
-        }
+
     }
 
     private void onTouchEventLine(MotionEvent event) {
@@ -181,10 +193,37 @@ public class PicEditView extends View {
 
     private void onDrawRect (Canvas canvas) {
 
+            drawRectangle(canvas, mPaint);
+
     }
 
     private void onTouchEventRectangle(MotionEvent event) {
+        mX = event.getX();
+        mY = event.getY();
+        Log.d("Touched: ", "(" + mX + ", " + mY + ")");
 
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //this.addPath();
+                oldMx = mX;
+                oldMy = mY;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //this.mPath.lineTo(mX, mY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawRectangle(mCanvas, mPaint);
+                break;
+        }
+        this.invalidate();
+    }
+
+    private void drawRectangle(Canvas canvas, Paint paint) {
+        float right = oldMx > mX ? oldMx : mX;
+        float left = oldMx > mX ? mX : oldMx;
+        float bottom = oldMy > mY ? oldMy : mY;
+        float top = oldMy > mY ? mY : oldMy;
+        canvas.drawRect(left, top , right, bottom, paint);
     }
 
     /*----------------------------------------------------------------------------------------------
